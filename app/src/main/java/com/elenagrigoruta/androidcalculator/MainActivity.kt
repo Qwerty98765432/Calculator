4db00849ea52fd888c10038aab0f0a07cbc33700
Elena Grigoruta <elena.grigoruta@gmail.com>
2023-01-17 00:01:33 +0200

    Initial commit

---
+++ app/src/main/java/com/elenagrigoruta/androidcalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.elenagrigoruta.androidcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.elenagrigoruta.androidcalculator.ui.theme.MaterialCalculatorTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialCalculatorTheme{
+            }
+        }
+    }
+}
